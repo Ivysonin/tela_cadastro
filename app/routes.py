f082718466306e6661cfcda0c848e6dd41597ad5
ivysonin <ivysonthauan4@gmail.com>
2025-06-28 11:37:01 -0300

    Removendo rotas sem funções para o propósito do projeto

--- app/routes.py
+++ app/routes.py
@@ -4,25 +4,11 @@ from app.forms import UsersForm
 from flask_login import login_user, login_required
 
 
-# Pág home
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-
 # Pág cadastrar
-@app.route('/cadastrar/', methods=['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def cadastrar():
     form = UsersForm()
     if form.validate_on_submit():
         user = form.save()
         login_user(user, remember=True)
-        return redirect(url_for('end'))
     return render_template('cadastrar.html', form=form)
-
-
-# Pág end
-@app.route('/end/')
-@login_required
-def end():
-    return render_template('end.html')
