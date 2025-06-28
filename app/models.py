1d56ddcde4139cade27debe06103e42acb572765
ivysonin <ivysonthauan4@gmail.com>
2025-06-28 09:45:34 -0300

    Adicionando campo de senha + pegando o usuário conforme o ID

--- app/models.py
+++ app/models.py
@@ -1,7 +1,12 @@
-from app import db
+from app import db, login_manager
+from flask_login import UserMixin
 
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(user_id)
 
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String, nullable=False)
     email =  db.Column(db.String, nullable=False)
+    senha =  db.Column(db.String, nullable=False)
